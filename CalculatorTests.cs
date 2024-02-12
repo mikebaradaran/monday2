c844981fc52234e2741454bfc8590e15bba0f534
mikebaradaran <mike.baradaran@qa.com>
2024-02-12 15:16:37 +0000

    first commit

---
+++ CalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTestWith2PositiveNumbers()
+        {
+            Calculator calc = new Calculator();
+
+            int res = calc.Add(1, 2);
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod()]
+        public void AddTestWith2NegativeNumbers()
+        {
+            Calculator calc = new Calculator();
+
+            int res = calc.Add(-100, -2);
+            Assert.AreEqual(-102, res);
+        }
+
+        [TestMethod()]
+        public void AddTestWith2NumbersOneBeingGreaterThan100()
+        {
+            Calculator calc = new Calculator();
+
+            int res = calc.Add(101, -2);
+            Assert.AreEqual(99, res);
+        }
+
+
+        [TestMethod()]
+        public void MultiplyTestWith2Numbers()
+        {
+            Calculator calc = new Calculator();
+
+            int res = calc.Multiply(100, 2);
+            Assert.AreEqual(200, res);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp22Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp22Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ff83d2ac-e27a-45d7-9fca-7d00c18ae4b6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
